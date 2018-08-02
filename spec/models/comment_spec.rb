144d5636e15691c61ed6635bb8279fb752d6ca99
JP Lynch <JPLynch35@gmail.com>
2018-08-02 12:45:08 -0600

    add relationship model tests

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Comment, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:content) }
+    it { should validate_presence_of(:question_id) }
+    it { should validate_presence_of(:user_id) }
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:user) }
+    it { should belong_to(:question) }
+    it { should have_many(:sub_comments) }
+  end
+
+  describe 'instance methods' do
+    
+  end
+
+  describe 'class methods' do
+    
+  end
+end
