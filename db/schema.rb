e4bb34d17ba9b5b3bfade06eb3e98331d2c200f7
JP Lynch <JPLynch35@gmail.com>
2018-08-03 15:27:48 -0600

    add feature tests for visitor and user visiting questions index, add questions index view

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20180803133747) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
+  add_foreign_key "comments", "questions"
   add_foreign_key "comments", "users"
   add_foreign_key "questions", "users"
   add_foreign_key "sub_comments", "comments"
---
+++ spec/features/user_can_see_question_index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'a default user' do
+  describe 'visiting the questions index page' do
+    before :each do
+      user1 = User.create(email: 'Bob@gmail.com', first_name: 'Bob', last_name: 'Smith', password: 'secret')
+      user2 = User.create(email: 'Jill@gmail.com', first_name: 'Jill', last_name: 'Smith', password: 'secret')
+      @question1 = user1.questions.create(content: 'How do people train for a marathon?')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user2)
+    end
+    it 'can see a list of all the parking lot questions' do
+      visit questions_path
+
+      expect(page).to have_content('Parking Lot')
+      expect(page).to have_content(@question1.content)
+    end
+  end
+end
