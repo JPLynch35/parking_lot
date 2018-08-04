8531a7aea6991ca7987809f317c8fd9220217ebb
JP Lynch <JPLynch35@gmail.com>
2018-08-04 13:42:30 -0600

    add feature tests for editing questions

--- spec/features/user_can_see_question_show_spec.rb
+++ spec/features/user_can_see_question_show_spec.rb
@@ -13,13 +13,24 @@ describe 'user visits question show page' do
       expect(page).to have_content(@question1.content)
       expect(page).to_not have_content(@question2.content)
     end
+    it 'cannot see edit buttons for any questions' do
+      visit question_path(@question1)
+
+      expect(page).to_not have_button('Edit')
+
+      visit question_path(@question2)
+
+      expect(page).to_not have_button('Edit')
+    end
   end
 
   context 'logged in as as a default user' do
     before :each do
       @user1 = User.create(email: 'Bob@gmail.com', first_name: 'Bob', last_name: 'Smith', password: 'secret')
+      @user2 = User.create(email: 'Jill@gmail.com', first_name: 'Jill', last_name: 'Smith', password: 'secret')
       @question1 = @user1.questions.create(content: 'How do people train for a marathon?')
       @question2 = @user1.questions.create(content: 'How do you win a pizza eating contest?')
+      @question3 = @user2.questions.create(content: 'How do you make millions of dollars?')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
     end
     it 'can see the question' do
@@ -27,6 +38,19 @@ describe 'user visits question show page' do
 
       expect(page).to have_content(@question1.content)
       expect(page).to_not have_content(@question2.content)
+    end
+    it 'can see edit buttons only for questions user made' do
+      visit question_path(@question1)
+
+      expect(page).to have_button('Edit')
+
+      visit question_path(@question2)
+
+      expect(page).to have_button('Edit')
+
+      visit question_path(@question3)
+
+      expect(page).to_not have_button('Edit')
     end
   end
 end
