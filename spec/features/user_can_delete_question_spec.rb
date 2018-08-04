38a03c4d4632ec8ba4c1eea6cbfa03646174e7cd
JP Lynch <JPLynch35@gmail.com>
2018-08-04 14:42:50 -0600

    add feature test for admin question delete

--- spec/features/user_can_delete_question_spec.rb
+++ spec/features/user_can_delete_question_spec.rb
@@ -18,4 +18,28 @@ describe 'user visits question edit page' do
       expect(page).to_not have_content(@question1.content)
     end
   end
+
+  context 'logged in as as an admin user' do
+    before :each do
+      @user1 = User.create(email: 'Bob@gmail.com', first_name: 'Bob', last_name: 'Smith', password: 'secret')
+      @user2 = User.create(email: 'Jill@gmail.com', first_name: 'Jill', last_name: 'Smith', password: 'secret', role: 1)
+      @question1 = @user1.questions.create(content: 'How do people train for a marathon?')
+      @question2 = @user1.questions.create(content: 'How do you win a pizza eating contest?')
+      @question3 = @user2.questions.create(content: 'How do you make millions of dollars?')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user2)
+    end
+    it 'can successfully delete any question' do
+      visit question_path(@question1)
+      click_on 'Delete'
+
+      expect(current_path).to eq(questions_path)
+      expect(page).to_not have_content(@question1.content)
+
+      visit question_path(@question2)
+      click_on 'Delete'
+
+      expect(current_path).to eq(questions_path)
+      expect(page).to_not have_content(@question2.content)
+    end
+  end
 end
