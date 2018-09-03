eed8aa690784410a84ccac30843d585da2126825
JP Lynch <JPLynch35@gmail.com>
2018-09-02 20:52:24 -0600

    remove welcome controller, root to questions#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'welcome#index'
+  get '/', to: 'questions#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
